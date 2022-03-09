1c02a23beb9f157cf7a3308669da101957b2eae0
Pat L <pleong@vmware.com>
2022-03-09 15:58:01 -0600

    Added copied code from online example

--- src/main/java/com/killchain/apollo/controllers/UserController.java
+++ src/main/java/com/killchain/apollo/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.killchain.apollo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,8 +19,29 @@ import java.util.Map;
 @CrossOrigin
 public class UserController {
 
+    @Autowired
+    final private OAuth2AuthorizedClientService authorizedClientService;
+
+    public UserController(OAuth2AuthorizedClientService authorizedClientService) {
+        this.authorizedClientService = authorizedClientService;
+    }
+
     @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
+    public Map<String, Object> user(HttpServletRequest httpServletRequest, OAuth2AuthenticationToken authentication) {
+        if (authentication == null) {
+            HashMap<String, Object> map = new HashMap<>();
+
+            map.put("login", "/login");
+        }
+        else {
+            OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
+            String userInfoEndpointUri = client.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri();
+
+            if (userInfoEndpointUri.equals("")){
+
+            }
+        }
+
+        return Collections.singletonMap("name", "");
     }
 }
